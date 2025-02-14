36dd9c460cc78fcd73a116253970befa55e9ec3e
Talib Kareem <taazkareem@icloud.com>
2025-02-13 23:00:10 -0500

    Updated twitter.js file

--- src/twitter.js
+++ src/twitter.js
@@ -3,3 +3,21 @@ const { Scraper } = require('agent-twitter-client');
 const scraper = new Scraper();
 
 // Add Twitter API credentials and other necessary code here
+
+// Login to Twitter
+scraper.login('username', 'password')
+  .then(() => {
+    console.log('Logged in successfully');
+  })
+  .catch((err) => {
+    console.error('Error logging in:', err);
+  });
+
+// Get tweets from a user
+scraper.getTweets('twitterdev', 10)
+  .then((tweets) => {
+    console.log('Tweets:', tweets);
+  })
+  .catch((err) => {
+    console.error('Error getting tweets:', err);
+  });
